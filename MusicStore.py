4af5efb027a7567c6a1d81da43490b31f4cdfe32
Phong Vang <phong.vang5@gmail.com>
2018-04-19 15:14:15 -0500

    Update 4.19
    3:15PM

--- MusicStore.py
+++ MusicStore.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+from passlib.hash import sha256_crypt
 
 #check connection
 try:
@@ -13,7 +14,16 @@ custCollection = db.Customers
 invColl = db.Inventory
 cartColl = db.ShoppingCart
 
+
+print("\nEnter 1 to Log in OR Enter 2 to Create an Account: ")
+
+def createCustomer():
+    db.custCollection.insert()
+    
+
+    
 #print customers
 doc = custCollection.find()
 for data in doc:
-    print(data)
+    print(data['firstName'], data['lastName'], data['pw'])
+    
