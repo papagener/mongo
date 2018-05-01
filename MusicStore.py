e442ec9112ec7ac9f3b1e00c9ec613951425ecf5
Freeman <bi5137pp@bi5137ppm7.workstations.winona.edu>
2018-05-01 04:24:37 -0500

    Ok, let's try this
    heeeere we go

--- MusicStore.py
+++ MusicStore.py
@@ -1,17 +1,57 @@
+#Title: MusicStore.py
+#Description: The purpose of this program is to simulate a music store that sells physical copies
+# of music (we'll say vinyl records since it's 2018).  Our clients want an online purchasing system that
+# allows users to register.  Once registered (and with a payment method on file) clients may purchase
+# physical copies which they may then pick up.
+#
+
 import pymongo
 from pymongo import MongoClient
 from passlib.hash import sha256_crypt
 
-#We need to have a connection, because right now we have conn.MovieStore
-#with no host
-
 
 #check connection
+
+#try MongoClient() first, which is PyMongo
+#pass parameters String name, int portNo 
 try:
     conn = MongoClient('localhost', 27017) #make sure the port number is correct
     print("Connection Successful")
 except:
     print("Connection Unsuccessful. Try again later.")
+
+
+#Connection to database made
+#Select database
+#MusicStore (CHANGE AS NEEDED)
+db = conn.MusicStore
+
+#all collections are occurring in MusicStore
+#select (or create) collection Customers
+#MusicStore/MovieStore CHANGE AS NEEDED
+db = conn.MovieStore
+
+customers = db.Customers
+#select (or create) collection Inventory
+inventory = db.Inventory
+#select (or create) collection ShoppingCart
+shoppingCart = db.ShoppingCart
+#select (or create) collection TEST_Customer
+testCust = db.TEST_Customer
+
+#set different flags
+outerLoopFlag = True;
+
+innerLoopFlag2 = True;
+
+
+#-------------------------------------------------------------------------------------------
+#
+#This section is for our different functions, creating them now and putting them together
+#seems like a nice way to organize them.  If there are more in the future, we can add them
+#here later as well.
+#
+#-------------------------------------------------------------------------------------------
 
 #MusicStore/MovieStore CHANGE AS NEEDED
 db = conn.MovieStore
@@ -88,48 +128,39 @@ def list():
     for data in doc:
         print(data['firstName'], data['lastName'], data['pw'])
 
-#Setup a menu to go through
+#Set up a menu to go through
 #Unlike Java, Python does not offer us a
 #switch statement
 #so we've just had to make a few different if statements here
-print("\nMain Menu")
-print("1. Login \n2. Create an Account ")
-choice = int(input())
-if choice == 1:
-    email = input("Enter your email: ")
-    pw = input("Enter your password: ")   
-    loginCustomer(email, pw)
-    #print(sha256_crypt.verify(pw, pw))
-    #once logged in enter a while loop for 
+while outerLoopFlag == True:
+    print("\nMain Menu")
+    print("Enter 1 to Login. Enter 2 to Create an Account: ")
+    choice = int(input())
 
-
-if choice == 2:
-    firstName = input("Enter your first name: ")
-    lastName = input("Enter your last name: ")
-    email = input("Enter your e-mail: ")
-    password = input("Enter a password: ")   
-    #hashpw = sha256_crypt.encrypt(password)
-    #hashing verifcation not working. Keeping password unhashed.
-    
-    #will have to index e-mail since that is unique
-    findUser = db.Customers.find_one({"email" : email})
-    if findUser is not None:
-        print("E-mail address already exists in database! Please register a different e-mail address!")
-    else:
-        createCustomer()        
+    #2nd level indent, inside of outer while loop        
+    if choice == 2:
+        innerLoopFlag = True;
+        while innerLoopFlag == True:
+            firstName = input("Enter your first name: ")
+            lastName = input("Enter your last name: ")
+            email = input("Enter your e-mail: ")
+            password = input("Enter a password: ")   
+            hashpw = sha256_crypt.encrypt(password)
             
-    result = db.Customers.create_index([('email', pymongo.ASCENDING)], unique = True)
-    sorted(list(db.Customers.index_information()))
-    
-    print("Welcome!")
-
-if choice == 3:
-    pw = input("Enter a password: ")
-    password = sha256_crypt.encrypt(pw)
-    print(password)
-    test = input("Enter pw: ")
-    print(sha256_crypt.verify(test, password))
-    #list()
+            #will have to index e-mail since that is unique
+            #if email is found, thus "is not None", give error message
+            findUser = db.Customers.find_one({"email" : email})
+            
+            if findUser is None:
+                createCustomer() 
+                result = db.Customers.create_index([('email', pymongo.ASCENDING)], unique = True)
+                print("Welcome!")
+                break;
+            else:
+                #previously gave information that email was already registered, elected not to give
+                #that information freely
+                print("Invalid entry") 
+                       
     
 
 
